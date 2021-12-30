52eab87e5e30e86f447a5cb26a2981d1cc7cdf52
asmitakoirala18 <asmita.koirala@csu.fullerton.edu>
2021-12-30 06:45:10 -0800

    acccessModifier

--- src/day2/Student.java
+++ src/day2/Student.java
@@ -20,18 +20,34 @@ public class Student {
     String phoneNumber;
     String dob;
     String sClass;
-    String rollNumber;
+    private String rollNumber;
     float height;
     String address;
     String idNumber;
     String email;
-    String username;
+    protected String username;
     String password;
     String emergencyContactNumber;
     String socialSecurityNumber;
 
 
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.name = "Ram";
+
+
+        Student student2 = new Student();
+        student2.name = "Asmi";
+
+        System.out.println(student1.name);
+        System.out.println(student1.rollNumber);
+
+
+    }
+
+
 }
+
 // behaviour
 
 // present
