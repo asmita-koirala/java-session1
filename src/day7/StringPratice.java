d52cfed0236b8b6a708251d6698769f144688b08
asmitakoirala18 <asmita.koirala@csu.fullerton.edu>
2022-01-25 07:59:28 -0800

    StringPractise

--- src/day7/StringPratice.java
+++ src/day7/StringPratice.java
@@ -1,4 +1,39 @@
 package day7;
 
+import java.util.logging.Logger;
+
 public class StringPratice {
+
+    public static void main (String[] args){
+        String name = "Asmita";
+
+        String lowerCaseString = name.toUpperCase();
+        Logger.getGlobal().info(lowerCaseString);
+
+//        String stringsubname = name.substring();
+//        Logger.getGlobal().info(subStringname);
+
+//        String New = name.toUpperCase();
+//        Logger.getGlobal().info(lowerCaseString);
+
+        String one= "one";
+        String anotherOne = "one";
+        String differentOne= "one two".substring(0,3);
+
+        Logger.getGlobal().info(String.valueOf(one == differentOne));
+        Logger.getGlobal().info(String.valueOf(one.equals(differentOne)));
+
+        String yes = "yes";
+        String userYes = "Yes";
+//        String lowercaseYes = userYes.toLowerCase();
+        //equalIgnoreCase use
+        Logger.getGlobal().info(String.valueOf("yes".equalsIgnoreCase(userYes)));
+
+
+
+
+
+    }
+
+
 }
